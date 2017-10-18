21c7c74521a2584c09c7c3605f9b41a5c9138a48
MyWorld88ggc <myworld88ggc@gmail.com>
2017-10-18 15:09:20 +0800

    update

--- MCActivity/src/test/java/HelloWorldTest.java
+++ MCActivity/src/test/java/HelloWorldTest.java
@@ -16,47 +16,53 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 import sun.management.counter.Variability;
+
 //import org.joda.time.DateTime;
 public class HelloWorldTest {
 
-	
-
-//	@Test
-//	public void test() {
-//		// //fail("Not yet implemented");
-//		// 创建流程引擎
-//		ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
-//				.buildProcessEngine();
-//
-//		// 部署流程文件
-//		RepositoryService repositoryService = processEngine.getRepositoryService();
-//		repositoryService.createDeployment().addClasspathResource("diagrams/MCLeaveProcess.bpmn").deploy();
-//
-//		// 验证已经部署的流程
-//		ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().singleResult();
-//
-//		String helloBpmnKey = processDefinition.getKey();
-//		System.out.println(helloBpmnKey);
-//
-//		// 启动流程实例
-//		RuntimeService runtimeService = processEngine.getRuntimeService();
-//		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(helloBpmnKey);
-//
-//		assertNotNull(processInstance);
-//
-//		System.out.println("pid=" + processInstance.getId() + "pdid=" + processInstance.getProcessDefinitionId());
-//
-//	}
+	// @Test
+	// public void test() {
+	// // //fail("Not yet implemented");
+	// // 创建流程引擎
+	// ProcessEngine processEngine =
+	// ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration()
+	// .buildProcessEngine();
+	//
+	// // 部署流程文件
+	// RepositoryService repositoryService =
+	// processEngine.getRepositoryService();
+	// repositoryService.createDeployment().addClasspathResource("diagrams/MCLeaveProcess.bpmn").deploy();
+	//
+	// // 验证已经部署的流程
+	// ProcessDefinition processDefinition =
+	// repositoryService.createProcessDefinitionQuery().singleResult();
+	//
+	// String helloBpmnKey = processDefinition.getKey();
+	// System.out.println(helloBpmnKey);
+	//
+	// // 启动流程实例
+	// RuntimeService runtimeService = processEngine.getRuntimeService();
+	// ProcessInstance processInstance =
+	// runtimeService.startProcessInstanceByKey(helloBpmnKey);
+	//
+	// assertNotNull(processInstance);
+	//
+	// System.out.println("pid=" + processInstance.getId() + "pdid=" +
+	// processInstance.getProcessDefinitionId());
+	//
+	// }
 
 	@Test
 	public void testStartReport() {
 
-		
-		
 	}
-	
+
 	/** 获得流程引擎 */
-	ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	// ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+	// ProcessEngine processEngine =
+	// ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+	ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault()
+			.buildProcessEngine();
 	String deploymentId;
 
 	@Test
@@ -69,95 +75,84 @@ public class HelloWorldTest {
 		Deployment deployment = processEngine.getRepositoryService()// 获取流程定义和部署相关的Service
 				.createDeployment()// 创建部署对象
 
-				.addClasspathResource("diagrams/StartReport12.bpmn")
-				.addClasspathResource("diagrams/StartReport12.png").deploy();// 完成部署
+				.addClasspathResource("diagrams/StartReport12.bpmn").addClasspathResource("diagrams/StartReport12.png")
+				.deploy();// 完成部署
 
 		deploymentId = deployment.getId();
-		
+
 		System.out.println("部署ID：" + deployment.getId());// 部署ID:1
 		System.out.println("部署时间：" + deployment.getDeploymentTime());// 部署时间
 		System.out.println("TenantId：" + deployment.getTenantId());// 部署时间
 		System.out.println("***************启动开工报告流程******************");
 
-		//设置参数
+		// 设置参数
 		Map<String, Object> startVariable = new HashMap<String, Object>();
 		startVariable.put("ProjectManger", "Kermit");
 		startVariable.put("AreaProjectManger", "WillGao");
 		startVariable.put("StartDate", "2017-03-09");
-		
-		
-		//启动流程
+
+		// 启动流程
 		String processDefinitionKey = "StartReport";// 流程定义的key,也就是bpmn中存在的ID
 		ProcessInstance pi = processEngine.getRuntimeService()// 管理流程实例和执行对象，也就是表示正在执行的操作
 				.startProcessInstanceByKey(processDefinitionKey, startVariable);//// 按照流程定义的key启动流程实例
 
-		//输出流程实例属性
+		// 输出流程实例属性
 		System.out.println("流程实例ID：" + pi.getId());// 流程实例ID：101
 		System.out.println("流程实例ID：" + pi.getProcessInstanceId());// 流程实例ID：101
 		System.out.println("流程实例ID:" + pi.getProcessDefinitionId());// myMyHelloWorld:1:4
 
 		System.out.println("***************开工报告审批******************");
 
-		
 		List<Task> tasks = processEngine.getTaskService()// 与任务相关的Service
 				.createTaskQuery()// 创建一个任务查询对象
 				.processInstanceId(pi.getId()).list();
 
-		
-		assertEquals(tasks.size(),1);
+		assertEquals(tasks.size(), 1);
 		if (tasks != null && tasks.size() > 0) {
 			for (Task task : tasks) {
 				ShowTaskInfo(task);
 			}
 		}
-		//设置变量，完成开工报告审批，审批结果设置为False
+		// 设置变量，完成开工报告审批，审批结果设置为False
 		String taskID = tasks.get(0).getId();
-		Map<String, Object> variables = new HashMap<String, Object>();		
+		Map<String, Object> variables = new HashMap<String, Object>();
 		variables.put("ApproveResult", "false");
 		processEngine.getTaskService().complete(taskID, variables);
-		
-		
+
 		System.out.println("***************开工报告修改******************");
 
 		Task updateStartReportTask = processEngine.getTaskService()// 与任务相关的Service
 				.createTaskQuery()// 创建一个任务查询对象
-				.processInstanceId(pi.getId())
-				.singleResult();
-		
+				.processInstanceId(pi.getId()).singleResult();
+
 		ShowTaskInfo(updateStartReportTask);
 		variables.clear();
 		variables.put("StartDate", "2017-03-09");
 
 		processEngine.getTaskService().complete(updateStartReportTask.getId(), variables);
-		
+
 		System.out.println("***************区域领导再次审批******************");
 
 		Task areaProjectManagerTask = processEngine.getTaskService()// 与任务相关的Service
 				.createTaskQuery()// 创建一个任务查询对象
-				.processInstanceId(pi.getId())
-				.singleResult();
-		
+				.processInstanceId(pi.getId()).singleResult();
+
 		ShowTaskInfo(areaProjectManagerTask);
 		variables.clear();
 		variables.put("ApproveResult", "true");
 
-		
 		processEngine.getTaskService().complete(areaProjectManagerTask.getId(), variables);
-		
 
 		System.out.println("*************** 流程结束 *****************");
-		
+
 		tasks = processEngine.getTaskService()// 与任务相关的Service
 				.createTaskQuery()// 创建一个任务查询对象
 				.processInstanceId(pi.getId()).list();
-		
+
 		assertEquals(0, tasks.size());
 
 	}
 
-	
-	
-	
 	private void ShowTaskInfo(Task task) {
 		System.out.println("#####################################");
 		// task.setAssignee("Kermit");
@@ -169,8 +164,5 @@ public class HelloWorldTest {
 		System.out.println("流程实例ID:" + task.getProcessInstanceId());
 		System.out.println("#####################################");
 	}
-	
-	
-	
-	
+
 }
